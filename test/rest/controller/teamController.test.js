717a5de5f5ac047f15cf0a57ca9f0aa8daa37e7e
Felipanjos <fanjos.santos@outlook.com>
2025-09-09 08:01:16 -0300

    feat: adicionando auth JWT aos testes

--- test/rest/controller/teamController.test.js
+++ test/rest/controller/teamController.test.js
@@ -8,16 +8,28 @@ const app = require('../../../app');
 // Mock
 const teamService = require('../../../service/teamService');
 
+// Auth
+let token;
+
 describe('Team Controller', () => {
+  before(async () => {
+    const resposta = await request(app).post('/login').send({
+      username: 'ash_ketchum',
+      password: 'pikachu123',
+    });
+
+    token = `${resposta.body.tokenType} ${resposta.body.token}`;
+  });
+
   describe('POST /teams', async () => {
     it('Quando não informo usuário e nome do time, recebo 400', async () => {
-      const resposta = await request(app).post('/teams').send({});
+      const resposta = await request(app).post('/teams').set('Authorization', token).send({});
       expect(resposta.status).to.equal(400);
       expect(resposta.body).to.have.property('error', 'Username e teamName são obrigatórios');
     });
 
     it('Quando informo usuário e nome do time válidos, recebo 201', async () => {
-      const resposta = await request(app).post('/teams').send({
+      const resposta = await request(app).post('/teams').set('Authorization', token).send({
         username: 'ash_ketchum',
         teamName: 'Meu Novo Time',
       });
@@ -28,10 +40,14 @@ describe('Team Controller', () => {
       const teamServiceMock = sinon.stub(teamService, 'createTeam');
       teamServiceMock.throws(new Error('Username e teamName são obrigatórios'));
 
-      const resposta = await request(app).post('/teams').send({
-        username: 'ash_ketchum',
-        teamName: 'Meu Novo Time',
-      });
+      const resposta = await request(app)
+        .post('/teams')
+        .set('Authorization', token)
+
+        .send({
+          username: 'ash_ketchum',
+          teamName: 'Meu Novo Time',
+        });
 
       expect(resposta.status).to.equal(400);
       expect(resposta.body).to.have.property('error', 'Username e teamName são obrigatórios');
@@ -44,10 +60,14 @@ describe('Team Controller', () => {
         teamName: 'Meu Novo Time',
       });
 
-      const resposta = await request(app).post('/teams').send({
-        username: 'eunaoexisto',
-        teamName: 'Meu Novo Time',
-      });
+      const resposta = await request(app)
+        .post('/teams')
+        .set('Authorization', token)
+
+        .send({
+          username: 'eunaoexisto',
+          teamName: 'Meu Novo Time',
+        });
 
       expect(resposta.status).to.equal(201);
       expect(resposta.body.username).to.equal('ash_ketchum');
--- test/rest/external/teamExternal.test.js
+++ test/rest/external/teamExternal.test.js
@@ -6,27 +6,41 @@ use(chaiExclude);
 
 describe('Teams', () => {
   describe('POST /teams', () => {
+    before(async () => {
+      const resposta = await request('http://localhost:3000').post('/login').send({
+        username: 'ash_ketchum',
+        password: 'pikachu123',
+      });
+
+      token = `${resposta.body.tokenType} ${resposta.body.token}`;
+    });
+
     it('Quando informo valores válidos ao adicionar um Pokémon ao time, recebo 200', async () => {
-        const postTeamsPokemon = require('../fixture/requisicoes/postTeamsPokemon.json');
-        const respostaEsperada = require('../fixture/respostas/quandoAdicionoPokemonComValoresValidosRecebo200.json');
+      const postTeamsPokemon = require('../fixture/requisicoes/postTeamsPokemon.json');
+      const respostaEsperada = require('../fixture/respostas/quandoAdicionoPokemonComValoresValidosRecebo200.json');
 
-        const resposta = await request('http://localhost:3000').post('/teams/pokemon').send(postTeamsPokemon);
+      const resposta = await request('http://localhost:3000')
+        .post('/teams/pokemon')
+        .set('Authorization', token)
 
-        expect(resposta.status).to.equal(200);
-        expect(resposta.body.pokemons).to.contain('Venusaur');
-        expect(resposta.body)
-            .excluding('size')
-            .to.deep.equal(respostaEsperada);
+        .send(postTeamsPokemon);
+
+      expect(resposta.status).to.equal(200);
+      expect(resposta.body.pokemons).to.contain('Venusaur');
+      expect(resposta.body).excluding('size').to.deep.equal(respostaEsperada);
     });
 
     it('Quando adiciono um Pokémon sem informar o nome, recebo 400', async () => {
-        const postTeamsPokemon = require('../fixture/requisicoes/postTeamsPokemon.json');
-        delete postTeamsPokemon.pokemonName;
-        
-        const resposta = await request('http://localhost:3000').post('/teams/pokemon').send(postTeamsPokemon);
+      const postTeamsPokemon = require('../fixture/requisicoes/postTeamsPokemon.json');
+      delete postTeamsPokemon.pokemonName;
 
-        expect(resposta.status).to.equal(400);
-        expect(resposta.body).to.have.property('error', 'Username, teamName e pokemonName são obrigatórios');
+      const resposta = await request('http://localhost:3000')
+        .post('/teams/pokemon')
+        .set('Authorization', token)
+        .send(postTeamsPokemon);
+
+      expect(resposta.status).to.equal(400);
+      expect(resposta.body).to.have.property('error', 'Username, teamName e pokemonName são obrigatórios');
     });
   });
 });
